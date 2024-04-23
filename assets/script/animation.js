636c0664796e826f5a4f966714d98df2e3791a6e
kaisar <kaisar0354@gmail.com>
2024-04-23 17:23:43 +0700

    add new page

---
+++ assets/script/animation.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $("#luffy").click(function(){
+    $("#luffy-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#zoro").click(function(){
+    $("#zoro-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#sanji").click(function(){
+    $("#sanji-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#chopper").click(function(){
+    $("#chopper-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#nami").click(function(){
+    $("#nami-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#brook").click(function(){
+    $("#brook-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#franky").click(function(){
+    $("#franky-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#robin").click(function(){
+    $("#robin-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#ussop").click(function(){
+    $("#ussop-bio").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#jinbei").click(function(){
+    $("#jinbei-bio").slideToggle("slow");
+    });
+});
+
